d3ab4f34110607af048ae833333d3fcfb4975ee4
Moheb619 <6moheb19@gmail.com>
2023-06-25 16:16:07 +0600

    nav fixed

--- src/app/components/common/Nav/Nav.tsx
+++ src/app/components/common/Nav/Nav.tsx
@@ -84,7 +84,9 @@ const Nav = () => {
           </ul>
         </div>
 
-        <a className="btn btn-ghost normal-case text-xl">ICICT4SD</a>
+        <Link href={"/"} className="btn btn-ghost normal-case text-xl">
+          ICICT4SD
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
