251ec50e44a1cf6aeccc55ed392cb8e6affa3a68
Moheb619 <6moheb19@gmail.com>
2023-08-10 20:27:47 +0600

    Keynote updated

--- src/app/components/PageComponents/EventSpeakers/EventSpeakers.tsx
+++ src/app/components/PageComponents/EventSpeakers/EventSpeakers.tsx
@@ -3,6 +3,7 @@ import DavidBrown from "public/img/keynote/David Brown.jpg";
 import KamalZuhairiBinZamli from "public/img/keynote/Kamal Zuhairi Bin Zamli.jpg";
 import LindaDaniela from "public/img/keynote/Linda Daniela.jpg";
 import DrSatyaPrasadMajumder from "public/img/keynote/Dr. Satya Prasad Majumder.jpg";
+import ProfJahangirHossain from "public/img/keynote/Prof Jahangir Hossain.jpg";
 import DrShahjahanMahmud from "public/img/speakers/Dr Shahjahan Mahmud.jpg";
 import YasirAzman from "public/img/speakers/Yasir Azman.jpg";
 import dummyImages from "public/img/dummyImages/dummy-profile-pic-male.jpg";
@@ -11,6 +12,7 @@ const keynoteSpeakers = [
   { name: "Professor Ts. Dr. Kamal Zuhairi Bin Zamli", bio: "Deputy Vice-Chancellor (Research & Innovation), University of Malaysia Pahang", url: KamalZuhairiBinZamli },
   { name: "David Brown", bio: "Professor in Interactive Systems for Social Inclusion, School of Science & Technology, Nottingham Trent University", url: DavidBrown },
   { name: "Dr. Satya Prasad Majumder", bio: "VC, BUET", url: DrSatyaPrasadMajumder },
+  { name: "Prof. Jahangir Hossain", bio: "Professor School of Electrical and Data Engineering", url: ProfJahangirHossain },
 ];
 const invitedSpeakers = [
   { name: "Dr. Shahjahan Mahmud", bio: "Chairman, BCSCL", url: DrShahjahanMahmud },
@@ -26,7 +28,7 @@ const EventSpeakers = () => {
         <div className="my-5 flex flex-wrap justify-center">
           {keynoteSpeakers.map((speaker, index) => {
             return (
-              <div key={speaker.name} className="mx-2 md:mx-5 my-2 md:my-0">
+              <div key={speaker.name} className="mx-2 md:mx-5 my-2 md:my-3">
                 <ImageCard title={"keynote"} name={speaker.name} bio={speaker.bio} url={speaker.url} />
               </div>
             );
@@ -38,7 +40,7 @@ const EventSpeakers = () => {
         <div className="my-5 flex flex-wrap justify-center">
           {invitedSpeakers.map((speaker, index) => {
             return (
-              <div key={index} className="mx-2 md:mx-5 my-2 md:my-0">
+              <div key={index} className="mx-2 md:mx-5 my-2 md:my-3">
                 <ImageCard title={"speaker"} name={speaker.name} bio={speaker.bio} url={speaker.url} />
               </div>
             );
